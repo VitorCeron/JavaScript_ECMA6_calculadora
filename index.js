a5f9fbc5ec87714e801c54c229941de7d3168e78
Vitor Ceron <vitor.ceron1997@gmail.com>
2019-04-06 14:47:21 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+document.getElementById("formCalcular").addEventListener("submit", function(event){
+    
+    let numero1 = parseFloat(document.getElementById('numero1').value);
+    let numero2 = parseFloat(document.getElementById('numero2').value);
+    let operacao = document.querySelector('input[name=operacao]:checked').value;
+    
+    var resultado = 0;
+    if( operacao === 'adicao' ){
+        resultado = numero1 + numero2;
+    } else if( operacao === 'subtracao' ){
+        resultado = numero1 - numero2;
+    } else if( operacao === 'multiplicacao' ){
+        resultado = numero1 * numero2;
+    } else if( operacao === 'divisao' ){
+        if( numero2 == 0 ){
+            alert('Não existe divisão por zero, selecionar outro número.');
+            return false;
+        } else {
+            resultado = numero1 / numero2;
+        }
+
+    } else {
+        alert('Operacao não localizada.');
+        return false;
+    }
+
+    document.getElementById('resultado').value = resultado;
+    
+    event.preventDefault();
+});
